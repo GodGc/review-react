3c04b3799a7d2eaaf807f6e8d3029d8445be553f
GodGc <zuitianlai@vip.qq.com>
2021-02-26 18:01:24 +0800

    demo: core-part done

--- src/pages/eventHandle/index.jsx
+++ src/pages/eventHandle/index.jsx
@@ -1,39 +1,70 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, Suspense } from "react";
+const Order = React.lazy(() => import("../order"));
 import ReactDOM from "react-dom";
 import "./index.scss";
 
 function EventHandle(props) {
     const data = 0;
-    const number = [1,2,3,4,5,6];
-    const numberDom = number.map(n=><p key={n}>{n}</p>)
+    const number = [1, 2, 3, 4, 5, 6];
+    const numberDom = number.map((n) => <p key={n}>{n}</p>);
+    const [isGoing, setIsGoing] = useState(true);
+    const [numberOfGuests, setNumberOfGuests] = useState(2);
+
+    function handleInputChange(e) {
+        const { name, checked = false, value = undefined } = e.target;
+        if (name == "isGoing") {
+            setIsGoing(checked);
+        }
+        if (name == "numberOfGuests") {
+            setNumberOfGuests(value);
+        }
+    }
     return (
         <div className="txt-18">
             I'm on.
-            <br/>
+            <br />
             <ActionLink />
-            <br/>
+            <br />
             {data && <h1> 我是Hsoc9xGu</h1>}
             {numberDom}
+            {number.map((n) => {
+                return <b key={n}>{n * 2}、</b>;
+            })}
+            <form>
+                <label>
+                    参与:
+                    <input name="isGoing" type="checkbox" checked={isGoing} onChange={handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    来宾人数:
+                    <input name="numberOfGuests" type="number" value={numberOfGuests} onChange={handleInputChange} />
+                </label>
+            </form>
+            {/* Suspense 组件是react内置的 */}
+            <Suspense fallback={<div>Loading...</div>}>
+                <Order />
+            </Suspense>
         </div>
     );
 }
 
-function ActionLink (){
+function ActionLink() {
     // useState的返回值是一个数组：【0】- flag的值，【1】- 改变flag值的方法
     // 为方便，使用ES6解构的方式获取
     const [flag, setFlag] = useState(false);
-    function handleClick (e){
+    function handleClick(e) {
         e.preventDefault(); // e是一个合成事件
-        console.log('The link was Clicked');
+        console.log("The link was Clicked");
 
         // 改变flag值
         setFlag(!flag);
     }
     return (
-        <a href="#" onClick={handleClick}>{flag?'no Wraning':'Wraning~'}</a>
-    )
+        <a href="#" onClick={handleClick}>
+            {flag ? "no Wraning" : "Wraning~"}
+        </a>
+    );
 }
-
-
 
 export default EventHandle;
